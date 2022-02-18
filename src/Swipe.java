7dcab075583515ea9bd480306da39cb9ed5e46f7
Ali Home <rezatrue@yahoo.com>
2022-02-18 20:41:11 +0600

    lecture: 23 gesture swipe

--- src/Swipe.java
+++ src/Swipe.java
@@ -1,8 +1,14 @@
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.LongPressOptions;
+import io.appium.java_client.touch.offset.ElementOption;
 
 public class Swipe extends Base{
 
@@ -16,6 +22,16 @@ public class Swipe extends Base{
 		driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
 		driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
 		driver.findElementByXPath("//*[@content-desc='10']").click();
+		
+		TouchAction touchAction = new TouchAction(driver);
+		LongPressOptions longPressOptions = new LongPressOptions();
+		ElementOption elementOption = new ElementOption();
+		 
+		WebElement firstElement = driver.findElementByXPath("//*[@content-desc='45']");
+		WebElement secondElement = driver.findElementByXPath("//*[@content-desc='15']");
+		touchAction.longPress(longPressOptions.withElement(elementOption.withElement(firstElement))
+				.withDuration(Duration.ofSeconds(2))).moveTo(elementOption.withElement(secondElement)).release();
+		
 		
 	}
 }
